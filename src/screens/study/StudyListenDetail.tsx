83edcfb1bb1f7dc17ee32c8343b7d24538e20480
khanhnh <khanhnh@vpbanks.com.vn>
2024-12-04 15:07:50 +0700

    update

--- src/screens/study/StudyListenDetail.tsx
+++ src/screens/study/StudyListenDetail.tsx
@@ -1,4 +1,4 @@
-import { View } from "react-native";
+import { Pressable, View } from "react-native";
 import { Card, Text } from "../../components/commons";
 import Icon from "react-native-vector-icons/Feather";
 import TextInput from "../../components/commons/TextInput";
@@ -6,6 +6,7 @@ import { getArrayRange } from "../../utils";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { ScrollView } from "react-native-gesture-handler";
 import Button from "../../components/commons/Button";
+import Sound from 'react-native-sound';
 
 import RNFS from 'react-native-fs';
 
@@ -13,8 +14,8 @@ const listSentences = getArrayRange(0, 40)
 
 function StudyListenDetail({ route }: { route?: any}) {
   async function handleDownload() {
-    const url = 'https://www.example.com/example.pdf';
-    const filePath = RNFS.DocumentDirectoryPath + '/example.pdf';
+    const url = 'https://cdn.pixabay.com/download/audio/2024/11/20/audio_1b5311d24a.mp3?filename=space-266642.mp3';
+    const filePath = RNFS.DocumentDirectoryPath + '/example.mp3';
 
     console.log('RNFS.DocumentDirectoryPath', RNFS.DocumentDirectoryPath)
 
@@ -37,6 +38,69 @@ function StudyListenDetail({ route }: { route?: any}) {
       });
   }
 
+  const whoosh = new Sound(RNFS.DocumentDirectoryPath + '/example.mp3', undefined, (error) => {
+    console.log(11111111111)
+    if (error) {
+      console.log('failed to load the sound', error);
+      return;
+    }
+
+    // // console.log('duration in seconds: ' + whoosh.getDuration() + 'number of channels: ' + whoosh.getNumberOfChannels());
+
+    // // Play the sound with an onEnd callback
+    // whoosh.play((success) => {
+    //   if (success) {
+    //     console.log('successfully finished playing');
+    //   } else {
+    //     console.log('playback failed due to audio decoding errors');
+    //   }
+    // });
+  });
+
+  async function handlePlayAudio() {
+    // const pathFile = RNFS.DocumentDirectoryPath + '/example.mp3';
+    // const isExist = await RNFS.exists(pathFile)
+
+    // if (isExist){
+    //   console.log("BLAH EXISTS");
+    // } else {
+    //   console.log("BLAH DOES NOT EXIST");
+    // }
+
+    // console.log(pathFile)
+
+    whoosh.play((success) => {
+      if (success) {
+        console.log('successfully finished playing');
+      } else {
+        console.log('playback failed due to audio decoding errors');
+      }
+    });
+
+    // const whoosh = new Sound('https://file-examples.com/wp-content/storage/2017/11/file_example_MP3_700KB.mp3', (error) => {
+    //   console.log(11111111111)
+    //   if (error) {
+    //     console.log('failed to load the sound', error);
+    //     return;
+    //   }
+
+    //   // console.log('duration in seconds: ' + whoosh.getDuration() + 'number of channels: ' + whoosh.getNumberOfChannels());
+
+    //   // Play the sound with an onEnd callback
+    //   whoosh.play((success) => {
+    //     if (success) {
+    //       console.log('successfully finished playing');
+    //     } else {
+    //       console.log('playback failed due to audio decoding errors');
+    //     }
+    //   });
+    // });
+  }
+
+  async function handlePauseAudio() {
+    whoosh.pause()
+  }
+
   return (
     <View style={{ backgroundColor: '#000', flex: 1 }}>
       <Card style={{ marginTop: 8 }}>
@@ -45,6 +109,9 @@ function StudyListenDetail({ route }: { route?: any}) {
       </Card>
       <Card>
         <Button title="Tải xuống" onPress={handleDownload} />
+      </Card>
+      <Card>
+        <Button title="Dừng" onPress={handlePauseAudio} />
       </Card>
       <SafeAreaProvider>
         <SafeAreaView style={{ flex: 1, marginTop: 8, marginBottom: 8 }} edges={['top']}>
@@ -73,7 +140,9 @@ function StudyListenDetail({ route }: { route?: any}) {
                       <View style={{ flex: 1 }}>
                         <TextInput showMessageError={false} placeholder="Vui lòng nhập những gì bạn nghe được" />
                       </View>
-                      <Icon name="play-circle" size={20} color={"#fff"} />
+                      <Pressable onPress={handlePlayAudio}>
+                        <Icon name="play-circle" size={20} color={"#fff"} />
+                      </Pressable>
                     </View>
                     <View style={{ display: 'flex', flexDirection: 'row' }}>
                       <View style={{ width: 28 }}></View>
